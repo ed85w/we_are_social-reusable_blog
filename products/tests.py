8b84773ab8e429e58b6f0990fb061d3672c81883
ed85w <ed.walker@hotmail.co.uk>
2017-07-30 12:42:33 +0100

    all tests working

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from views import all_products
 
 # Create your tests here.
+class productPageTest(TestCase):
+    def test_products_page_resolves(self):
+        products_page = resolve('/products/')
+        self.assertEqual(products_page.func, all_products)
+
