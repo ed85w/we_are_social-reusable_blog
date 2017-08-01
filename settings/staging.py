f10f2f5a3ae35aaa626eb1b79b4a9ac87abce91b
ed85w <ed.walker@hotmail.co.uk>
2017-08-01 20:09:53 +0100

    gunicorn installed in env

---
+++ settings/staging.py
@@ -0,0 +1,19 @@
+from base import *
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+#  Stripe environment variables
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_WPgPzAQeCfea08WkskzTqxii')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_6vCTHyEBILOhXzg3gfMd9U8w')
+
+# paypal environment variables
+SITE_URL = 'https://eds-we-are-social.herokuapp.com/'
+PAYPAL_NOTIFY_URL = 'https://eds-we-are-social.herokuapp.com/a-very-hard-to-guess-url/'
+PAYPAL_RECEIVER_EMAIL = 'ed.walker.merchant@hotmail.co.uk'
